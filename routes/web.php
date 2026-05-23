ffb28c2e3eda3feb0cba2065cd1f8831f5502eda
Amandys2m <amanda.s02m@gmail.com>
2026-05-23 14:21:33 -0300

    CRUD especialidades

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientesController;
+use App\Http\Controllers\EspecialidadesController;
 use App\Models\Cliente;
+use App\Models\Especialidade;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,4 +16,8 @@ Route::post('/clientes/novo/{id?}', [ClientesController::class, 'salvar'])->name
 Route::get('/clientes/edit/{id}', [ClientesController::class, 'edit'])->name('cliente.edit');
 Route::get('/clientes/delete/{id}', [ClientesController::class, 'delete'])->name('cliente.delete');
 
-
+Route::get('/especialidades', [EspecialidadesController::class, 'listar']);
+Route::get('/especialidades/nova', [EspecialidadesController::class, 'nova'])->name('especialidade.nova');
+Route::post('/especialidades/nova/{id?}', [EspecialidadesController::class, 'salvar'])->name('especialidade.salvar');
+Route::get('/especialidades/editar/{id}', [EspecialidadesController::class, 'editar'])->name('especialidade.editar');
+Route::get('/especialidades/delete/{id}', [EspecialidadesController::class, 'delete'])->name('especialidade.delete');
